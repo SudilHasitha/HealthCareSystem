002114f53ed61024ad560c40d45e449baccadd8a
Udara <Udara@DESKTOP-6J4U4V7>
2020-04-17 15:02:03 -0700

    Hospital first commit

--- HealthCareSystem/src/com/HospitalService.java
+++ HealthCareSystem/src/com/HospitalService.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
@@ -13,71 +14,124 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import controller.HospitalDBHandler;
 import model.Hospital;
 
-@Path("hospital")
-public class HospitalService implements HospitalInterface{
+@Path("/hospital")
+public class HospitalService {
 	 
-	
-	HospitalDBHandler Hosrepo = new HospitalDBHandler();
+	HospitalDBHandler HosObj = new HospitalDBHandler();
 	
 	@GET
-	@Path("/readAllHos")
+	@Path("readallHos")
 	@Produces(MediaType.TEXT_HTML)
 	public String readAllHospital() {
 		System.out.println("getAllHospital is called");
-		return Hosrepo.readAllHospital() ;
+		return HosObj.readAllHospital() ;
 	}
-	   
+	
+	
 	@POST
-	@Path("/InsertHos")
-	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-	public Hospital HospitalInsert(Hospital hos) {
+	@Path("insertHos")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String HospitalInsert(@FormParam("hospital_reg_no") String hospital_reg_no,
+			@FormParam("hos_name") String hos_name,
+			@FormParam("hos_type") String hos_type,
+			@FormParam("AddressLine1") String AddressLine1,
+			@FormParam("city") String city,
+			@FormParam("province") String province,
+			@FormParam("telephone") String telephone,
+			@FormParam("hospital_fee") String hospital_fee,
+			@FormParam("hos_password") String hos_password) 
+	{
 		
-		System.out.println(hos.getHospital_reg_no());
-		System.out.println(hos.getHos_name());
-		System.out.println(hos.gethos_type());
-		System.out.println(hos.getAddressLine1());
-		System.out.println(hos.getCity());
-		System.out.println(hos.getProvince());
-		System.out.println(hos.getTelephone());
-		System.out.println(hos.getHos_password());
-		System.out.println(hos.getHos_email() );
-		
-		Hosrepo.HospitalInsert(hos);
-		
-		return hos;
+		String output = HosObj.HospitalInsert(hospital_reg_no, hos_name, hos_type, AddressLine1,city,province,telephone,hospital_fee,hos_password);
+		return output;
 	}
 	
 	@PUT
-	@Path("/updateHos")
-	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-	public Hospital updateHos(Hospital hos) {
+	@Path("updatehos")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updateHos(String hosData) {
 		
-		System.out.println(hos.getHospital_reg_no());
-		System.out.println(hos.getHos_name());
-		System.out.println(hos.gethos_type());
-		System.out.println(hos.getAddressLine1());
-		System.out.println(hos.getCity());
-		System.out.println(hos.getProvince());
-		System.out.println(hos.getTelephone());
-		System.out.println(hos.getHos_password());
-		System.out.println(hos.getHos_email() );
-		
-			Hosrepo.updateHos(hos);
+		//Convert the input string to a JSON object
+		JsonObject HospObject = new JsonParser().parse(hosData).getAsJsonObject();
 			
-			return hos;
+		 String hospital_reg_no= HospObject.get("hospital_reg_no").getAsString();
+		 String hos_name= HospObject.get("hos_name").getAsString();
+		 String hos_type= HospObject.get("hos_type").getAsString();
+		 String AddressLine1= HospObject.get("AddressLine1").getAsString();
+		 String city= HospObject.get("city").getAsString();
+		 String province= HospObject.get("province").getAsString();
+		 String telephone= HospObject.get("telephone").getAsString();
+		 String hospital_fee = HospObject.get("hospital_fee").getAsString();
+
+		 String output = HosObj.updateHos(hospital_reg_no, hos_name, hos_type, AddressLine1, city, province, telephone, hospital_fee);
+		
+		 return output;
 	}
 	
- 
-	@DELETE
-	@Path("/del")
+	@GET
+	@Path("{reg}")
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.TEXT_HTML)
+	public String readHospital(@PathParam("reg") int id) {
+		System.out.println("getHospital is called");
+		String output = HosObj.readHospital(id);
+		return output;
+	
+	}
+	
+	/*@GET
+	@Path("dochos/{reg}")
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.TEXT_HTML)
+	public String readHospitalDoctersDetail(@PathParam("reg") int id) {
+		System.out.println("getHospital is called");
+		String output = HosObj.readHospitalDoctersDetail(id);
+		return output;
+	
+	}*/
+	@PUT
+	@Path("updatePassword")
 	@Consumes(MediaType.APPLICATION_JSON)
-	public String deleteHos(String id) {
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updateHosPassword(String hosData) {
 		
-		String output = Hosrepo.deleteHos(id);
+		//Convert the input string to a JSON object
+		JsonObject HospObject = new JsonParser().parse(hosData).getAsJsonObject();
+			
+		 String hospital_reg_no= HospObject.get("hospital_reg_no").getAsString();
+		 String hos_password = HospObject.get("hos_password").getAsString();
+
+		 String output = HosObj.updateHosPassword(hospital_reg_no, hos_password);
+		
+		 return output;
+	}
+	
+
+	@DELETE
+	@Path("deletehos")
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteHospital(String ID)
+	{
+		//Convert the input string to an XML document
+		Document doc = Jsoup.parse(ID, "", Parser.xmlParser());
+		
+		//Read the value from the element <itemID>
+		String hos_id = doc.select("hos_id").text();
+		
+		String output = HosObj.deleteHospital(hos_id);
 		return output;
 	}
 	
--- HealthCareSystem/src/controller/HospitalDBHandler.java
+++ HealthCareSystem/src/controller/HospitalDBHandler.java
@@ -21,7 +21,15 @@ public class HospitalDBHandler {
 		return con;
 	}
 	
-	public String HospitalInsert(Hospital hos) {
+	public String HospitalInsert(String hospital_reg_no,
+			 String hos_name,
+			 String hos_type,
+			 String AddressLine1,
+			 String city,
+			 String province,
+			 String telephone,
+			 String hospital_fee,
+			 String hos_password) {
 		
 		String output = "";
 
@@ -34,22 +42,21 @@ public class HospitalDBHandler {
 
 			// prepared statement
 			String query = "Insert into hospital (hospital_reg_no,hos_name,hos_type,"
-					+ "AddressLine1,city,province,telephone,hospital_fee,hos_password,hos_email) "
-					+ "values (?,?,?,?,?,?,?,?,?,?) ";
+					+ "AddressLine1,city,province,telephone,hospital_fee,hos_password) "
+					+ "values (?,?,?,?,?,?,?,?,?) ";
 
 			PreparedStatement preparedStatement = con.prepareStatement(query);
 
 			// binding values
-			preparedStatement.setString(1, hos.getHospital_reg_no());
-			preparedStatement.setString(2, hos.getHos_name());
-			preparedStatement.setString(3, hos.gethos_type());
-			preparedStatement.setString(4, hos.getAddressLine1());
-			preparedStatement.setString(5, hos.getCity());
-			preparedStatement.setString(6, hos.getProvince());
-			preparedStatement.setString(7, hos.getTelephone());
-			preparedStatement.setDouble(8, hos.getHospital_fee());
-			preparedStatement.setString(9, hos.getHos_password());
-			preparedStatement.setString(10, hos.getHos_email());
+			preparedStatement.setString(1, hospital_reg_no);
+			preparedStatement.setString(2, hos_name);
+			preparedStatement.setString(3, hos_type);
+			preparedStatement.setString(4, AddressLine1);
+			preparedStatement.setString(5, city);
+			preparedStatement.setString(6, province);
+			preparedStatement.setString(7, telephone);
+			preparedStatement.setDouble(8, Double.parseDouble(hospital_fee));
+			preparedStatement.setString(9, hos_password);
 			
 
 			// execution
@@ -61,14 +68,46 @@ public class HospitalDBHandler {
 		} catch (Exception e) {
 			output = "Error while inserting";
 			System.err.println(e.getMessage());
-			;
 		}
 
 		return output;
 
 	}
 	
-	public String updateHos(Hospital hos) {
+	public String getHosID(String reg_no) {
+		
+		String output = "";
+
+		try {
+			Connection con = connect();
+
+			if (con == null) {
+				return "Error while connecting to DB";
+			}
+			
+			String query = "Select hos_id from hospital WHERE hospital_reg_no =?";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			
+			preparedStmt.setString(1, reg_no);
+			
+			ResultSet rs = preparedStmt.executeQuery(query);
+			
+			String hos_id = Integer.toString(rs.getInt("hos_id"));
+				
+			output =hos_id;
+				
+			con.close();
+			}
+			catch (Exception e)
+			{
+				output = "Error while reading the Hos_id.";
+				System.err.println(e.getMessage());
+			}
+			return output;
+		}
+	
+	public String updateHos(String hospital_reg_no,	String hos_name,String hos_type,String AddressLine1,String city, String province, String telephone,String hospital_fee) 
+	{
 
 		String output = "";
 
@@ -80,23 +119,20 @@ public class HospitalDBHandler {
 			}
 
 			// prepared statement
-			String query = "UPDATE hospital SET hospital_reg_no=?,hos_name=?,hos_type=?,AddressLine1=?,city=?,province=?,telephone=?,hospital_fee=?,hos_password=?,hos_email=?"
-					+ " WHERE hos_id =?";
+			String query = "UPDATE hospital SET hos_name=?,hos_type=?,AddressLine1=?,city=?,province=?,telephone=?,hospital_fee=?"
+					+ " WHERE hospital_reg_no =?";
 
 			PreparedStatement preparedStatement = con.prepareStatement(query);
 
 			// binding values
-			preparedStatement.setString(1, hos.getHospital_reg_no());
-			preparedStatement.setString(2, hos.getHos_name());
-			preparedStatement.setString(3, hos.gethos_type());
-			preparedStatement.setString(4, hos.getAddressLine1());
-			preparedStatement.setString(5, hos.getCity());
-			preparedStatement.setString(6, hos.getProvince());
-			preparedStatement.setString(7, hos.getTelephone());
-			preparedStatement.setDouble(8, hos.getHospital_fee());
-			preparedStatement.setString(9, hos.getHos_password());
-			preparedStatement.setString(10, hos.getHos_email());
-			preparedStatement.setInt(11, hos.getHos_id());
+			preparedStatement.setString(1, hos_name);
+			preparedStatement.setString(2, hos_type);
+			preparedStatement.setString(3, AddressLine1);
+			preparedStatement.setString(4, city);
+			preparedStatement.setString(5, province);
+			preparedStatement.setString(6, telephone);
+			preparedStatement.setDouble(7, Double.parseDouble(hospital_fee));
+			preparedStatement.setString(8, hospital_reg_no);
 			// execution
 			preparedStatement.execute();
 			con.close();
@@ -112,7 +148,9 @@ public class HospitalDBHandler {
 		return output;
 	}
 	
-	public String deleteHos(String RegNo) {
+	public String updateHosPassword(String hospital_reg_no,String password) 
+	{
+
 		String output = "";
 
 		try {
@@ -123,85 +161,57 @@ public class HospitalDBHandler {
 			}
 
 			// prepared statement
-			String query = "Delete from hospital where  Hospital_reg_no= ?";
+			String query = "UPDATE hospital SET hos_password=?"
+					+ " WHERE hospital_reg_no =?";
 
 			PreparedStatement preparedStatement = con.prepareStatement(query);
 
 			// binding values
-			preparedStatement.setString(1,RegNo);
-
+			preparedStatement.setString(1, password);
+			preparedStatement.setString(2, hospital_reg_no);
 			// execution
 			preparedStatement.execute();
 			con.close();
 
-			output = "Deleted Successfully";
+			output = "Updated Successfully";
 
 		} catch (Exception e) {
-			output = "Error while Deleting";
+			output = "Error while updating";
 			System.err.println(e.getMessage());
+			;
 		}
 
 		return output;
 	}
 	
-	public String readHospital(String regno) {
+	public String deleteHospital(String ID)
+	{
 		String output = "";
-
-		try {
+		try
+		{
 			Connection con = connect();
-
-			if (con == null) {
-				return "Error while connecting";
+			if (con == null)
+			{
+				return "Error while connecting to the database for deleting."; 
 			}
-
-			// Prepare the html table to be displayed
-			output = "<table border=\"1\"><tr><th>Registration No</th><th>Name</th><th>Type</th>"
-					+ "<th>Addressline</th><th>city</th><th>province</th><th>Contact</th><th>Fee</th><th>E-mail</th></tr>";
-			
-			String query = "select * from hospital where hospital_reg_no = "+regno;
-			Statement statement = con.createStatement();
-			ResultSet resultSet = statement.executeQuery(query);
-
-			// iterate through the results
-			while (resultSet.next()) {
-				String hos_id = Integer.toString(resultSet.getInt(1));
-				String hospital_reg_no = resultSet.getString(2);
-				String hos_name = resultSet.getString(3);
-				String type_private = resultSet.getString(4);
-				String AddressLine1 = resultSet.getString(5);
-				String city = resultSet.getString(6);
-				String province = resultSet.getString(7);
-				String telephone = resultSet.getString(8);
-				String hospital_fee = Double.toString(resultSet.getDouble(9));
-				String hos_email = Double.toString(resultSet.getDouble(11));
-				
-				// add to html table
-				output += "<tr><td>" + hospital_reg_no + "</td>";
-				output += "<td>" + hos_name + "</td>";
-				output += "<td>" + type_private + "</td>";
-				output += "<td>" + AddressLine1 + "</td>";
-				output += "<td>" + city + "</td>";
-				output += "<td>" + province + "</td>";
-				output += "<td>" + telephone + "</td>";
-				output += "<td>" + hospital_fee + "</td>";
-				output += "<tr><td>" + hos_email + "</td>";
-
-				// buttons
-				output += "<td><input name=\"btnUpdate\" type=\"button\" value=\"Update\" class=\"btn btn-secondary\"></td>"
-						+ "<td><form method=\"post\" action=\"items.jsp\">"
-						+ "<input name=\"btnRemove\" type=\"submit\" value=\"Remove\" class=\"btn btn-danger\">"
-						+ "<input name=\"itemID\" type=\"hidden\" value=\"" + hos_id + "\">" + "</form></td></tr>";
-			}
-
+			// create a prepared statement
+			String query = "delete from hospital where hos_id=?";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			// binding values
+			preparedStmt.setInt(1, Integer.parseInt(ID));
+			// execute the statement
+			preparedStmt.execute();
 			con.close();
-
-			output += "</table>";
-		} catch (Exception e) {
-			output = "Error while reading";
+			output = "Deleted successfully";
+		}
+		catch (Exception e)
+		{
+			output = "Error while deleting the item.";
 			System.err.println(e.getMessage());
 		}
 		return output;
 	}
+	
 	
 	public String readAllHospital() {
 		String output = "";
@@ -215,7 +225,7 @@ public class HospitalDBHandler {
 
 			// Prepare the html table to be displayed
 			output = "<table border=\"1\"><tr><th>Registration No</th><th>Name</th><th>Type</th>"
-					+ "<th>Addressline</th><th>city</th><th>province</th><th>Contact</th><th>Fee</th><th>E-mail</th></tr>";
+					+ "<th>Addressline</th><th>city</th><th>province</th><th>Contact</th><th>Fee</th><th>Update</th><th>Remove</th></tr>";
 			
 			String query = "select * from hospital";
 			Statement statement = con.createStatement();
@@ -232,7 +242,7 @@ public class HospitalDBHandler {
 				String province = resultSet.getString(7);
 				String telephone = resultSet.getString(8);
 				String hospital_fee = Double.toString(resultSet.getDouble(9));
-				String hos_email = Double.toString(resultSet.getDouble(11));
+
 				
 				// add to html table
 				output += "<tr><td>" + hospital_reg_no + "</td>";
@@ -243,7 +253,7 @@ public class HospitalDBHandler {
 				output += "<td>" + province + "</td>";
 				output += "<td>" + telephone + "</td>";
 				output += "<td>" + hospital_fee + "</td>";
-				output += "<tr><td>" + hos_email + "</td>";
+		
 
 				// buttons
 				output += "<td><input name=\"btnUpdate\" type=\"button\" value=\"Update\" class=\"btn btn-secondary\"></td>"
@@ -262,7 +272,7 @@ public class HospitalDBHandler {
 		return output;
 	}
 	
-/*	public String readHospitalDoctersDetail() {
+	public String readHospital(int id) {
 		String output = "";
 
 		try {
@@ -271,35 +281,38 @@ public class HospitalDBHandler {
 			if (con == null) {
 				return "Error while connecting";
 			}
-
-			// Prepare the html table to be displayed
-			output = "<table border=\"1\"><tr><th>Registration No</th><th>Hospital Name</th><th>Hospital fee</th>"
-					+ "<th>Docter ID</th><th>Docter Name</th><th>Docter fee</th></tr>";
 			
-			String query = "select h.hospital_reg_no,h.hos_name,h.hospital_fee,d.doc_id,d.docname,d.docfee from hospital h,doctor d"
-					+ "where hos_id=?"
-					+ "order by h.hospital_reg_no";
+			String query = "select * from hospital where hos_id = " +id;
 			Statement statement = con.createStatement();
 			ResultSet resultSet = statement.executeQuery(query);
 
 			// iterate through the results
 			while (resultSet.next()) {
-				String hospital_reg_no = resultSet.getString(1);
-				String hos_name = resultSet.getString(2);
-				String hospital_fee = Double.toString(resultSet.getDouble(3));
-				String Docid = resultSet.getString(4);
-				String DocName = resultSet.getString(5);
-				String Docfee = Double.toString(resultSet.getDouble(6));
+				String hos_id = Integer.toString(resultSet.getInt(1));
+				String hospital_reg_no = resultSet.getString(2);
+				String hos_name = resultSet.getString(3);
+				String type_private = resultSet.getString(4);
+				String AddressLine1 = resultSet.getString(5);
+				String city = resultSet.getString(6);
+				String province = resultSet.getString(7);
+				String telephone = resultSet.getString(8);
+				String hospital_fee = Double.toString(resultSet.getDouble(9));
 				
-				// add to html table
-				output += "<tr><td>" + hospital_reg_no + "</td>";
-				output += "<td>" + hos_name + "</td>";
-				output += "<td>" + hospital_fee + "</td>";
-				output += "<td>" + Docid + "</td>";
-				output += "<td>" + DocName + "</td>";
-				output += "<td>" + Docfee + "</td>";
+				
+				// Prepare the html table to be displayed
+				output = "<table><tr><th> Hospital ID</th><td> : </td><td>"+ hos_id + "</td></tr>";
+				output += "<tr><th> Hospital hospital_reg_no</th><td> : </td><td>" + hospital_reg_no + "</td></tr>";
+				output += "<tr><th> Hospital Name</th><td> : </td><td>" + hos_name + "</td></tr>";
+				output += "<tr><th> Hospital Type</th><td> : </td><td>" + type_private + "</td></tr>";
+				output += "<tr><th> Hospital Address line 1</th><td> : </td><td>" + AddressLine1 + "</td></tr>";
+				output += "<tr><th> City</th><td> : </td><td>" + city + "</td></tr>";
+				output += "<tr><th> Province </th><td> : </td><td>" + province + "</td></tr>";
+				output += "<tr><th>Telephone</th><td> : </td><td>" + telephone + "</td></tr>";
+				output += "<tr><th> Hospital fee</th><td> : </td><td>" + hospital_fee + "</td></tr>";
+		
+	
 			}
-
+			
 			con.close();
 
 			output += "</table>";
@@ -309,7 +322,52 @@ public class HospitalDBHandler {
 		}
 		return output;
 	}
-	*/
+	
+/*	public String readHospitalDoctersDetail(int id) {
+		String output = "";
+
+		try {
+			Connection con = connect();
+
+			if (con == null) {
+				return "Error while connecting";
+			}
+			
+			String query = "select distinct  doctor.docid, hospital.hos_name,doctor.doc_name"
+					+ "from hospital "
+					+ " left join (visit cross join doctor) "
+					+ "on (hospital.hos_id = visit.hosid "
+					+ "and visit.docid = doctor.docid )"
+					+ "where hospital.hos_id=" +id;
+			
+			Statement statement = con.createStatement();
+			ResultSet resultSet = statement.executeQuery(query);
+
+			// iterate through the results
+			while (resultSet.next()) {
+				String hospital_reg_no = resultSet.getString(1);
+				String hos_name = resultSet.getString(2);
+				String docid =Integer.toString(resultSet.getInt(4));
+				String doc_name = resultSet.getString(3);
+				
+				// Prepare the html table to be displayed
+				output = "<table>";
+				output += "<tr><th> Hospital Name</th><td> : </td><td>" + hos_name + "</td></tr>";
+				output += "</table><table><tr><th>Docter id</th><th> DOcter name</th></tr>";
+				output += "<tr><td>" + docid + "</td><td>" + doc_name + "</td></tr>";
+	
+			}
+			
+			con.close();
+
+			output += "</table>";
+		} catch (Exception e) {
+			output = "Error while reading";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+	
 	/*public String readHospitalPatientDetail() {
 		String output = "";
 
